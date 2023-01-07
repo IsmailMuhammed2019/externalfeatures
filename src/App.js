aa6e9efe0bc877f84c4837e4311828d7ecc3c62c
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-07 16:40:28 +0100

    update state for values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, {}from 'react'
 import './App.css';
 
 function App() {
+  const [firstName, setFirstName] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className='App'>
       <form>
-        <input name='firstName' type='text' value id='firstname' />
-        <input name='email' type='text' value id='email' />
-        <button type='submit'>Add People</button>
+        <input 
+        name='firstName' 
+        type='text' 
+        placeholder='Type name here'
+        id='firstname'
+        value={firstName}
+        onChange={handleChange}
+        />
+        <input 
+        name='email'
+        type='text'
+        placeholder='Type email here'
+        id='email' 
+        value={email}
+        onChange={handleChange}
+        />
+        <button type='submit' onClick={handleSubmit}>Add People</button>
       </form>
       <h2>People's List</h2>
     </div>
