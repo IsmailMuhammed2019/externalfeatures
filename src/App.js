6f28307bd2f0f5d2e092f5e4571cba676f4aa058
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-07 15:54:43 +0100

    add form frame

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <form>
+        <input name='firstName' type='text' value id='firstname' />
+        <input name='email' type='text' value id='email' />
+        <button type='submit'>Add People</button>
+      </form>
+      <h2>People's List</h2>
     </div>
-  );
+  )
 }
 
 export default App;
