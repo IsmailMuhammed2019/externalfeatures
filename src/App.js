ca3cefe0a389f106501ee5968bf6f6d352ba827d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-07 16:45:01 +0100

    setup onChange function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {}from 'react'
+import React, {useState}from 'react'
 import './App.css';
 
 function App() {
   const [firstName, setFirstName] = useState("")
+  const [email, setEmail] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -11,23 +12,25 @@ function App() {
   return (
     <div className='App'>
       <form>
-        <input 
-        name='firstName' 
-        type='text' 
-        placeholder='Type name here'
-        id='firstname'
-        value={firstName}
-        onChange={handleChange}
+        <input
+          name='firstName'
+          type='text'
+          placeholder='Type name here'
+          id='firstname'
+          value={firstName}
+          onChange={(e) => setFirstName(() => e.target.value)}
         />
-        <input 
-        name='email'
-        type='text'
-        placeholder='Type email here'
-        id='email' 
-        value={email}
-        onChange={handleChange}
+        <input
+          name='email'
+          type='text'
+          placeholder='Type email here'
+          id='email'
+          value={email}
+          onChange={(e) => setEmail(() => e.target.value)}
         />
-        <button type='submit' onClick={handleSubmit}>Add People</button>
+        <button type='submit' onClick={handleSubmit}>
+          Add People
+        </button>
       </form>
       <h2>People's List</h2>
     </div>
