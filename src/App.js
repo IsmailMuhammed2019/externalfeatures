e90320980a8f615b6fe27b6b6c23d104b06980b7
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-10 10:17:08 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,69 @@
-import React, {useState}from 'react'
-import './App.css';
+import { type } from '@testing-library/user-event/dist/type'
+import React, {useState} from 'react'
+import './App.css'
 
-function App() {
-  const [firstName, setFirstName] = useState("")
-  const [email, setEmail] = useState("")
-  const [people, setPeople] =useState([])
+
+const App = () => {
+  const [person, setPerson] = useState({firstName:"", email: "", id: new Date().getTime().toString()})
+  const [people, setPeople] = useState([])
+
+  const handleChange = (e) => {
+              const { name, value } = e.target
+    setPerson((prev) => {
+      return {
+          ...prev, 
+          [name]: value
+      }
+    })
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(firstName && email){
-      const person = {id: new Date().getTime().toString(), firstName, email}
+    if (person.firstName && person.email){
       setPeople((prev) => {
-        return [...prev, person]})
-    }else{
-      alert("Please fill the form")
+        return [...prev, person]
+      })
+    }else {
+      alert("Please fill form")
     }
-    console.log(people)
-  }
 
+    setPerson(prev => {
+      return (
+        {...prev, firstName: '', email:''}
+      )
+    })
+  }
   return (
-    <div className='App'>
-      <form>
+    <div>
+      <form onSubmit={handleSubmit}>
         <input
+          type='text'
           name='firstName'
-          type='text'
+          value={person.firstName}
           placeholder='Type name here'
-          id='firstname'
-          value={firstName}
-          onChange={(e) => setFirstName(() => e.target.value)}
+          onChange={handleChange}
         />
         <input
+          type='email'
           name='email'
-          type='text'
+          value={person.email}
           placeholder='Type email here'
-          id='email'
-          value={email}
-          onChange={(e) => setEmail(() => e.target.value)}
+          onChange={handleChange}
         />
-        <button type='submit' onClick={handleSubmit}>
-          Add People
-        </button>
+        <button type='submit'>Submit</button>
       </form>
-      <h2>People's List</h2>
+      <h2>List of People</h2>
+      {people.map((person) => {
+        const {id, firstName, email} = person
+        return (
+          <div key={id}>
+            <h3>{firstName}</h3>
+            <p>{email}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
 
-export default App;
+export default App
